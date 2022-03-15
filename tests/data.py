3e1755f0b5a38fdce72025ea95fe1c632294155c
Junrong Yu <junrong.yu@uq.edu.au>
2022-03-16 09:01:51 +1000

    python list created to replace JSON

---
+++ tests/data.py
@@ -0,0 +1,9 @@
+properties = [
+    [
+        "plot-description-slope",
+        "shapes/plot-description/slope/shapes.ttl",
+        "shapes/plot-description/slope/valid.ttl",
+        "shapes/plot-description/slope/invalid.ttl",
+        7,
+    ]
+]
--- tests/test.py
+++ tests/test.py
@@ -2,17 +2,15 @@ import json
 from pyshacl import validate
 import re
 import pytest
+from data import properties
 
-with open("tests/test.json") as f:
-    data = json.loads(f.read())
-
-@pytest.mark.parametrize("name, shapes, valid, invalid, expected", data)
+@pytest.mark.parametrize("name, shapes, valid, invalid, expected", properties)
 def test_valid(name, invalid, shapes, valid, expected):
     r = validate(valid, shacl_graph=shapes, advanced=True)
     conforms, results_graph, results_text = r
     assert conforms == True
 
-@pytest.mark.parametrize("name, shapes, valid, invalid, expected", data)
+@pytest.mark.parametrize("name, shapes, valid, invalid, expected", properties)
 def test_invalid(name, invalid, shapes, valid, expected):
     r = validate(invalid, shacl_graph=shapes, advanced=True)
     conforms, results_graph, results_text = r
