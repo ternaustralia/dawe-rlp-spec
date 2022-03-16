79113c0e0b110eaa1036b71f314989bd7740dbb4
Junrong Yu <junrong.yu@uq.edu.au>
2022-03-16 10:27:59 +1000

    Get the number of errors from the results_graph

--- tests/test.py
+++ tests/test.py
@@ -3,6 +3,7 @@ from pyshacl import validate
 import re
 import pytest
 from data import properties
+import rdflib
 
 @pytest.mark.parametrize("name, shapes, valid, invalid, expected", properties)
 def test_valid(name, invalid, shapes, valid, expected):
@@ -14,6 +15,16 @@ def test_valid(name, invalid, shapes, valid, expected):
 def test_invalid(name, invalid, shapes, valid, expected):
     r = validate(invalid, shacl_graph=shapes, advanced=True)
     conforms, results_graph, results_text = r
-    results_text = results_text.splitlines()[2]
-    errors_num = int(re.findall(r"\d+", results_text)[0])
+    errors_num = int(results_graph.query(q).bindings[0][rdflib.term.Variable('count')])
     assert errors_num == expected
+
+q = """
+    PREFIX sh: <http://www.w3.org/ns/shacl#>
+
+    SELECT (count(distinct ?result) AS ?count)
+    WHERE {
+        ?graph a sh:ValidationReport ;
+               sh:conforms false ;
+               sh:result ?result .
+    }
+"""
