c3b37f8bf0815766261cb46f0d139bf62f13f312
Junrong Yu <junrong.yu@uq.edu.au>
2022-03-16 11:58:39 +1000

    file format changed

--- tests/test.py
+++ tests/test.py
@@ -4,18 +4,21 @@ from pyshacl import validate
 
 from manifests import test_cases
 
+
 @pytest.mark.parametrize("name, shapes, valid, invalid, expected", test_cases)
 def test_valid(name, invalid, shapes, valid, expected):
     r = validate(valid, shacl_graph=shapes, advanced=True)
     conforms, results_graph, results_text = r
     assert conforms == True
 
+
 @pytest.mark.parametrize("name, shapes, valid, invalid, expected", test_cases)
 def test_invalid(name, invalid, shapes, valid, expected):
     r = validate(invalid, shacl_graph=shapes, advanced=True)
     conforms, results_graph, results_text = r
-    errors_num = int(results_graph.query(q).bindings[0][rdflib.term.Variable('count')])
+    errors_num = int(results_graph.query(q).bindings[0][rdflib.term.Variable("count")])
     assert errors_num == expected
+
 
 q = """
     PREFIX sh: <http://www.w3.org/ns/shacl#>
