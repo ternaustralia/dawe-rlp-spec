8b9b7c3287b0362f855f8130351e9b166a2f19c1
Junrong Yu <junrong.yu@uq.edu.au>
2022-03-17 13:59:04 +1000

    update ontotools version

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,8 @@
+import turtle
 import pytest
 import rdflib
 from pyshacl import validate
+from ontotools.ontotools.functions.validate import validate_syntax
 
 from manifests import test_cases
 
@@ -18,6 +20,24 @@ def test_invalid(name, invalid, shapes, valid, expected):
     conforms, results_graph, results_text = r
     errors_num = int(results_graph.query(q).bindings[0][rdflib.term.Variable("count")])
     assert errors_num == expected
+
+
+@pytest.mark.parametrize("name, shapes, valid, invalid, expected", test_cases)
+def validate_valid(name, invalid, shapes, valid, expected):
+    result = validate_syntax(valid, "turtle")
+    assert result == True
+
+
+@pytest.mark.parametrize("name, shapes, valid, invalid, expected", test_cases)
+def validate_invalid(name, invalid, shapes, valid, expected):
+    result = validate_syntax(invalid, "turtle")
+    assert result == True
+
+
+@pytest.mark.parametrize("name, shapes, valid, invalid, expected", test_cases)
+def validate_shapes(name, invalid, shapes, valid, expected):
+    result = validate_syntax(shapes, "turtle")
+    assert result == True
 
 
 q = """
