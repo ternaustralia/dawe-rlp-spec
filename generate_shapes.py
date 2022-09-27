1de0b27f64b31f0cb5621df236a45a9325d47296
junrongYu <junrong.yu@uq.edu.au>
2022-09-27 09:43:09 +0000

    Added scripts for site visit invalid examples

--- generate_shapes.py
+++ generate_shapes.py
@@ -729,6 +729,75 @@ for property_uri in properties_collection_members:
         (invalid_value_type_uri, TERN.hasSiteVisit, URIRef("urn:test:site"))
     )
 
+    # Add the general content of site visit invalid examples in invalid_graph
+    invalid_site_visit_uri = URIRef(
+        "urn:test:"
+        + properties_collection_file_path
+        + ":invalid:"
+        + property_label_file_path
+        + ":site-visit"
+    )
+
+    invalid_graph.add((invalid_site_visit_uri, RDF.type, TERN.Observation))
+    invalid_graph.add((invalid_site_visit_uri, VOID.inDataset, invalid_in_dataset))
+    invalid_graph.add(
+        (
+            invalid_site_visit_uri,
+            RDFS.comment,
+            Literal("Invalid result - all observations must have a site vist."),
+        )
+    )
+
+    invalid_site_visit_feature_of_interest_bnode = BNode()
+    invalid_graph.add(
+        (
+            invalid_site_visit_uri,
+            SOSA.hasFeatureOfInterest,
+            invalid_site_visit_feature_of_interest_bnode,
+        )
+    )
+    invalid_graph.add(
+        (
+            invalid_site_visit_feature_of_interest_bnode,
+            RDF.type,
+            TERN.FeatureOfInterest,
+        )
+    )
+    invalid_graph.add(
+        (
+            invalid_site_visit_feature_of_interest_bnode,
+            VOID.inDataset,
+            invalid_in_dataset,
+        )
+    )
+    invalid_graph.add(
+        (
+            invalid_site_visit_feature_of_interest_bnode,
+            TERN.featureType,
+            URIRef(property_feature_type),
+        )
+    )
+
+    invalid_site_visit_result_bnode = BNode()
+    invalid_graph.add(
+        (invalid_site_visit_uri, SOSA.hasResult, invalid_site_visit_result_bnode)
+    )
+    invalid_graph.add((invalid_site_visit_result_bnode, RDF.type, TERN.Value))
+    invalid_graph.add(
+        (invalid_site_visit_result_bnode, SOSA.isResultOf, invalid_site_visit_uri)
+    )
+
+    invalid_graph.add(
+        (invalid_site_visit_uri, SOSA.ObservableProperty, URIRef(property_uri))
+    )
+    invalid_graph.add(
+        (invalid_site_visit_uri, SOSA.phenomenonTime, invalid_phenomenon_time)
+    )
+    invalid_graph.add((invalid_site_visit_uri, SOSA.resultTime, invalid_result_time))
+    invalid_graph.add(
+        (invalid_site_visit_uri, SOSA.usedProcedure, URIRef(protocol_module_uri))
+    )
+
     if URIRef(property_value_type) in [TERN.Integer, TERN.Float]:
         # Add the general content of value range validation in shapes_graph
         shapes_value_range_uri = URIRef(
