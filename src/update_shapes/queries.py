62792d6c0868406aa3d9078eaa6574242cce5873
Junrong Yu <junrong.yu@uq.edu.au>
2024-01-08 01:43:06 +0000

    Added scripts to generate shapes to check site visit existence

--- src/update_shapes/queries.py
+++ src/update_shapes/queries.py
@@ -3,6 +3,7 @@ from settings import dawe_endpoint, tern_endpoint
 
 import string
 
+
 def get_op_collection_members(properties_collection_uri):
 
     q_get_op_collection_members = """
@@ -26,21 +27,21 @@ def get_op_collection_members(properties_collection_uri):
 
     return op_collection_members
 
-def get_op_meta_info(property_uri, method_uri):
+def get_op_meta_info(property_uri, protocol_uri):
     q_get_op_info = """
     select ?featureType ?valueType {
         values ?property_uri {$property_uri}
-        values ?method_uri {$method_uri}
+        values ?protocol_uri {$protocol_uri}
         ?s <urn:property:featureType> ?featureType ;
         <urn:property:valueType> ?valueType ;
         <urn:property:observableProperty> ?property_uri ;
-        <urn:property:protocolModule> ?method_uri .
+        <urn:property:protocolModule> ?protocol_uri .
         }
     """
 
     q_get_op_info = string.Template(q_get_op_info).safe_substitute(
         property_uri=f"<{property_uri}>",
-        method_uri=f"<{method_uri}>"
+        protocol_uri=f"<{protocol_uri}>"
     )
 
     op_info_list = sparql_query(dawe_endpoint, q_get_op_info)["results"][
@@ -88,3 +89,42 @@ def get_feature_type_label(feature_type_uri):
     )["results"]["bindings"][0]["label"]["value"].replace("-", " ")
 
     return feature_type_label
+
+def get_protocol_label(protocol_uri):
+    q_get_protocol_label = """
+    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+    select ?label {
+        values ?protocol_uri {$protocol_uri}
+        ?protocol_uri skos:prefLabel ?label .
+    }
+    """
+
+    q_get_protocol_label = string.Template(q_get_protocol_label).substitute(
+        protocol_uri=f"<{protocol_uri}>"
+    )
+
+    protocol_label = sparql_query(dawe_endpoint, q_get_protocol_label)[
+        "results"
+    ]["bindings"][0]["label"]["value"]
+
+    return protocol_label
+
+
+def query_to_get_observation_general_info(op_uri):
+    q_shapes_get_general = """
+    PREFIX tern: <https://w3id.org/tern/ontologies/tern/>
+    PREFIX unit: <http://qudt.org/vocab/unit/>
+    PREFIX sosa: <http://www.w3.org/ns/sosa/>
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    select ?this {
+        values ?property_uri {$property_uri}
+        ?this a tern:Observation ;
+            sosa:observedProperty ?property_uri .
+    }
+    """
+
+    q_shapes_get_general = string.Template(q_shapes_get_general).substitute(
+        property_uri=f"<{op_uri}>"
+    )
+
+    return q_shapes_get_general
