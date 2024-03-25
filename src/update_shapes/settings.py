590d7a914b9248821440aafe6f02312233131702
Junrong Yu <junrong.yu@uq.edu.au>
2024-03-25 16:01:22 +1000

    In settings, added example values for phenomenon time, result time, dataset

--- src/update_shapes/settings.py
+++ src/update_shapes/settings.py
@@ -1,4 +1,4 @@
-from rdflib import Namespace, Literal
+from rdflib import Namespace, Literal, URIRef, XSD
 
 URNC = Namespace("urn:class:")
 URNP = Namespace("urn:property:")
@@ -10,3 +10,8 @@ dawe_endpoint = "https://graphdb.tern.org.au/repositories/dawe_vocabs_core"
 tern_endpoint = "https://graphdb.tern.org.au/repositories/tern_vocabs_core"
 
 source = Literal("Ecological Monitoring System Australia (EMSA) Protocols")
+phenomenon_time_example = URIRef("https://example.com/observation/examplePhenomenonTime")
+result_time_example = Literal(
+        "2022-09-27T05:38:47.117000+00:00", datatype=XSD.dateTime
+    )
+dataset_example = URIRef("https://example.com/dataset/1")
