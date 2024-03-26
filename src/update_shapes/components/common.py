cd1fa6d98bac7b1278bc5aed2c133441b4e6e51e
Junrong Yu <junrong.yu@uq.edu.au>
2024-03-26 16:57:18 +1000

    Formatted scripts, writing function to generate observation examples

--- src/update_shapes/components/common.py
+++ src/update_shapes/components/common.py
@@ -1,5 +1,15 @@
 from rdflib import RDF, SH, DCTERMS, BNode, Literal, URIRef, XSD
-from settings import URNP, URNC, source, REG
+from settings import (
+    URNP,
+    URNC,
+    source,
+    REG,
+    dataset_example,
+    phenomenon_time_example,
+    result_time_example,
+)
+from functions import generate_op_shapes_folder, generate_protocol_folder
+
 
 def add_common_properties_shapes(g, uri):
     g.add((uri, RDF.type, SH.PropertyShape))
@@ -9,13 +19,12 @@ def add_common_properties_shapes(g, uri):
 
     return g
 
+
 def generate_target_bnode(g, uri, query):
     target_bnode = BNode()
     g.add((uri, SH.target, target_bnode))
     g.add((target_bnode, RDF.type, SH.SPARQLTarget))
-    g.add(
-        (target_bnode, SH.select, Literal(query))
-    )
+    g.add((target_bnode, SH.select, Literal(query)))
 
     return g
 
@@ -23,15 +32,15 @@ def generate_target_bnode(g, uri, query):
 def generate_sparql_bnode(g, uri, query):
     sparql_bnode = BNode()
     g.add((uri, SH.sparql, sparql_bnode))
-    g.add(
-        (sparql_bnode, SH.select, Literal(query))
-    )
+    g.add((sparql_bnode, SH.select, Literal(query)))
 
     return g
 
 
-def generate_example_files_node(g, uri, op_collection_folder_path, op_shapes_folder_path):
-    
+def generate_example_files_node(
+    g, uri, op_collection_folder_path, op_shapes_folder_path
+):
+
     invalid_examples_link = Literal(
         "https://github.com/ternaustralia/dawe-rlp-spec/blob/main/shapes/"
         + op_collection_folder_path
@@ -49,20 +58,23 @@ def generate_example_files_node(g, uri, op_collection_folder_path, op_shapes_fol
         + "/valid.ttl",
         datatype=XSD.anyURI,
     )
-    
+
     example_files_node_uri = URIRef(
-        "urn:shapes:" + op_collection_folder_path + ":" + op_shapes_folder_path + ":examples"
+        "urn:shapes:"
+        + op_collection_folder_path
+        + ":"
+        + op_shapes_folder_path
+        + ":examples"
     )
 
     g.add((uri, URNP.examples, example_files_node_uri))
-    g.add(
-        (example_files_node_uri, URNP.invalidExample, invalid_examples_link)
-    )
+    g.add((example_files_node_uri, URNP.invalidExample, invalid_examples_link))
     g.add((example_files_node_uri, URNP.validExample, valid_examples_link))
 
     return g
 
-def link_shapes_file (g, uri, op_collection_folder_path, op_shapes_folder_path):
+
+def link_shapes_file(g, uri, op_collection_folder_path, op_shapes_folder_path):
     shapes_link = Literal(
         "https://github.com/ternaustralia/dawe-rlp-spec/blob/main/shapes/"
         + op_collection_folder_path
@@ -77,6 +89,32 @@ def link_shapes_file (g, uri, op_collection_folder_path, op_shapes_folder_path):
     return g
 
 
-def add_observation_examples():
+def add_observation_examples(
+    g,
+    validation_type,
+    protocol_label,
+    op_label,
+    example_type,
+    data_type,
+    value_type,
+    feature_type,
+    example_value,
+    op_uri,
+    protocol_uri,
+):
     # This function will add observation example nodes for both invalid and valid examples
-    pass
+    uri = URIRef(
+        "urn:test:"
+        + generate_protocol_folder(protocol_label)
+        + generate_op_shapes_folder(op_label)
+        + ":"
+        + validation_type
+        + ":"
+        + example_type
+    )
+
+    # TODO: need to write a function to generate comments
+    comment = validation_type + ""
+
+    g.add((uri, RDF.type, TERN.Observation))
+    g.add((uri, VOID.inDataset, dataset_example))
